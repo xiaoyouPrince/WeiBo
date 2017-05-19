8f190ca8116a7c856d65c528e49cb2b1cb52c4a0
xiaoyouPrince <xiaoyouPrince@163.com>
2017-05-20 01:15:06 +0800

    设置主页导航栏左右的item
    通过遍历构造函数，快速设置主页导航栏左右item

--- WeiBo/WeiBo/Classes/Home/HomeViewController.swift
+++ WeiBo/WeiBo/Classes/Home/HomeViewController.swift
@@ -13,7 +13,28 @@ class HomeViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        isLogin = true
+        
+        // 未登录时候的页面
         visitorView.addRotationAnim()
+        if !isLogin {
+            return
+        }
+        
+        // 登录之后的页面
+        setupNavigationBar()
+        
     }
+}
 
+
+extension HomeViewController{
+    
+    func setupNavigationBar() {
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "navigationbar_friendattention")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(imageName: "navigationbar_pop")
+
+        
+    }
 }
---
+++ WeiBo/WeiBo/Classes/Tools/UIBarButtonItem-Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIBarButtonItem-Extension.swift
+//  WeiBo
+//
+//  Created by 渠晓友 on 2017/5/20.
+//  Copyright © 2017年 xiaoyouPrince. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIBarButtonItem
+{
+    
+    /// 创建一个便利构造函数，通过一个image快速生成
+    convenience init(imageName : String) {
+        
+        let btn = UIButton()
+        btn.setImage(UIImage(named : imageName ), for: .normal)
+        btn.setImage(UIImage(named : imageName + "_highlighted" ), for: .highlighted)
+        btn.sizeToFit()
+        
+        self.init(customView: btn)
+        
+    }
+}
