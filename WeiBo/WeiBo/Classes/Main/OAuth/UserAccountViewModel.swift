67c89d11dab3b6d5fc6418e5d926b280d90f0425
xiaoyouPrince <xiaoyouPrince@163.com>
2017-05-25 10:54:12 +0800

    对 UserAccount 视图模型的封装
    创建 UserAccountViewModel 来统一管理 UserAccount的一些基本功能。实际上是一个工具类

---
+++ WeiBo/WeiBo/Classes/Main/OAuth/UserAccountViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  UserAccountViewModel.swift
+//  WeiBo
+//
+//  Created by 渠晓友 on 2017/5/25.
+//  Copyright © 2017年 xiaoyouPrince. All rights reserved.
+//
+//  封装一个 UserAccountTool 管理 UserAccount
+
+import UIKit
+
+class UserAccountViewModel: NSObject {
+    
+    // 封装成单例，方便存取
+    static let shareInstance : UserAccountViewModel = UserAccountViewModel()
+    
+    // MARK: - 定义属性
+    var account : UserAccount?
+    
+    // MARK: - 计算属性
+    var accountPath : String {
+        
+        var path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        path = (path as NSString).strings(byAppendingPaths: ["account.plist"]).first!
+        
+        print("本沙盒路径 : " + path)
+        
+        return path
+    }
+    
+    // MARK: - 计算属性，是否过期计算是否登录
+    var isLogin : Bool {
+        
+        // 如果没有读到 account 信息直接 false
+        guard let userAccount = account else { return false }
+        
+//        if let expiresDate = userAccount.expires_date {
+//            
+//            if expiresDate.compare(Date()) == ComparisonResult.orderedDescending {
+//                return true
+//            }else{
+//                return false
+//            }
+//            
+//        }else {
+//            return false
+//        }
+        
+        guard let expiresDate = userAccount.expires_date else {
+            return false
+        }
+        
+        if expiresDate.compare(Date()) == ComparisonResult.orderedDescending {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
+    
+    // 重写新建
+    override init() {
+        super.init()
+        
+        // 读取 account 信息
+        account = NSKeyedUnarchiver.unarchiveObject(withFile: accountPath) as? UserAccount
+
+    }
+    
+    /// 归档user信息
+    func archiveAccount(_ account : UserAccount) {
+        
+        // 提供一个方法来归档信息
+        NSKeyedArchiver.archiveRootObject(account, toFile: accountPath)
+    }
+    
+    
+
+}
