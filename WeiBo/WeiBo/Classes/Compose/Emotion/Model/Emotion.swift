81335590c75eb01eb4246616c7a7b462d70bb895
xiaoyouPrince <xiaoyouPrince@163.com>
2017-06-11 01:37:08 +0800

    EmojiCode的数据处理
    Emoji是全球通用的，处理方法都一样，根据emoji对应的code，用Scanner读取对应的16进制数值，根据它创建对应的emoji对应的s
    tring

--- WeiBo/WeiBo/Classes/Compose/Emotion/Model/Emotion.swift
+++ WeiBo/WeiBo/Classes/Compose/Emotion/Model/Emotion.swift
@@ -12,6 +12,25 @@ class Emotion: NSObject {
     
     // MARK: - 属性
     var code : String? /// emoji 对应的code
+    {
+        didSet{
+            // 处理emojiCode
+            
+            guard let code = code else {
+                return
+            }
+            
+            let scanner = Scanner(string: code)
+            
+            var value : UInt32 = 0
+            scanner.scanHexInt32(&value)
+            
+            let scalar = UnicodeScalar(value)
+            let c = Character(scalar!)
+            
+            emojiCode = String(c)
+        }
+    }
     var png : String?  /// 普通表情对应的png
     {
         didSet{
@@ -27,17 +46,19 @@ class Emotion: NSObject {
     
     // MARK: - 数据处理
     var pngPath : String?
+    var emojiCode : String?
     
     init(dict : [String : Any]){
         super.init()
         setValuesForKeys(dict)
+        
     }
     
     override func setValue(_ value: Any?, forUndefinedKey key: String) {    }
     
     
     override var description: String{
-        return dictionaryWithValues(forKeys: ["code","pngPath","chs"]).description
+        return dictionaryWithValues(forKeys: ["emojiCode","pngPath","chs"]).description
     }
 
 }
